5f439efb38afd1619d93986ae4d37e038236adfc
nadim00100 <nadim.almasri0@gmail.com>
2025-06-24 15:48:14 +0200

    Create the Route and the Function def add()

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask,render_template
+from flask import Flask, render_template, request
 import json
 
+
 app = Flask(__name__)
+
+
+
 
 
 def load_posts():
@@ -13,6 +17,13 @@ def index():
     blog_posts = load_posts()
     return render_template('index.html', posts=blog_posts)
 
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        # We will fill this in the next step
+        pass
+    return render_template('add.html')
+
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
